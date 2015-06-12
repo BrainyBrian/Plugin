027f653a5179b87a01eae7e66668c5fd28d27a09
BrainyBrian <dgramopadhye@gmail.com>
2015-06-12 16:23:18 -0400

    Update Samosa.php

--- src/dGRAMOP-Development/Samosa/Samosa.php
+++ src/dGRAMOP-Development/Samosa/Samosa.php
@@ -1,3 +1,17 @@
 <?php
-// The File
+
+namespace dGRAMOP-Development\Samosa;
+
+use pocketmine\plugin\PluginBase;
+
+class MyPlugin extends PluginBase{
+
+    public function onEnable(){
+        $this->getLogger()->info("----------------- Samosa has Started --------------------");
+    }
+
+    public function onDisable(){
+        $this->getLogger()->info("---------------- Samosa has been stopped ---------------");
+    }
+}
 ?>
