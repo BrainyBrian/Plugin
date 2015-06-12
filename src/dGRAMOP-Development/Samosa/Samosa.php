28155a34d72e0a5848ee108a1273828c8eda463c
BrainyBrian <dgramopadhye@gmail.com>
2015-06-12 17:09:12 -0400

    Update Samosa.php

--- src/dGRAMOP-Development/Samosa/Samosa.php
+++ src/dGRAMOP-Development/Samosa/Samosa.php
@@ -3,13 +3,34 @@
 namespace dGRAMOP-Development\Samosa;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command
 
-class MyPlugin extends PluginBase{
-
+class Samosa extends PluginBase{
     public function onEnable(){
         $this->getLogger()->info("----------------- Samosa has Started --------------------");
     }
+    
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+    if(strtolower($command->getName()) === "/samosa"){
+        
+        if(strtolower($args[0]) === "server-send"){
+            $everything=array_shift($args);
+            $this->getLogger()->info("Incomming Message from ". $sender." >>> ". implode($everything, " "));
+            return true;
+        }
+        if(strtolower($args[0]) === "server-send"){
+            $everything=array_shift($args);
+            $this->getLogger()->info("Incomming Message from ". $sender." >>> ". implode($everything, " "));
+            return true;
+        }
+        
+    }
+    }
+    
 
+    return false;
+    }
+    
     public function onDisable(){
         $this->getLogger()->info("---------------- Samosa has been stopped ---------------");
     }
